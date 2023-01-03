76e7a89014bb3cfaa2023317aec9f32c2ef74902
noramlnine <codejamgg@gmail.com>
2023-01-03 20:41:37 +0900

    230103_KDH_Enemy_DieAni

--- Source/HandToHand_MOVE/Private/EnemyFSM.cpp
+++ Source/HandToHand_MOVE/Private/EnemyFSM.cpp
@@ -284,7 +284,7 @@ void UEnemyFSM::OnDamageProcess()
 
 		// 캡슐 충돌체 비활성화
 		me->GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-
+		me->GetMesh()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 		// 죽음 애니메이션 재생
 		anim->PlayDamageAnim(TEXT("Die"));
 	}
