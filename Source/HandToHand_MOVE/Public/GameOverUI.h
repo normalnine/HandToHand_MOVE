aa52b215b3ebe4001cf617238d9d5475e7f6aa85
noramlnine <codejamgg@gmail.com>
2023-01-12 15:42:05 +0900

    230112_KDH_UI

--- Source/HandToHand_MOVE/Public/GameOverUI.h
+++ Source/HandToHand_MOVE/Public/GameOverUI.h
@@ -13,5 +13,22 @@ UCLASS()
 class HANDTOHAND_MOVE_API UGameOverUI : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+protected:
+	//BeginPlay 와 같은 동작
+	virtual void NativeConstruct() override;
+
+public:
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+		class UButton* btnRetry;
+
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+		class UButton* btnQuit;
+
+public:
+	UFUNCTION()
+		void Retry();
+
+	UFUNCTION()
+		void Quit();
 };
